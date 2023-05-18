a14aeb496a8c513b2e3e256859690bc10384bfe4
Dmitriy Melnik <dmitriy.melnik@joy-dev.com>
2023-05-18 11:42:44 +0300

    inital commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Мельник Дмитрий on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
